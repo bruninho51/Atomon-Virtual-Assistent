2b4590bfaead77f1038cc60116557a0908b2d800
Bruno Mendes <bruno.mendes51@hotmail.com>
2021-10-07 22:54:21 -0300

    fix: throw error when knowledge is not exists

--- src/data/repositories/elasticsearch-delete-knowledge.repository.ts
+++ src/data/repositories/elasticsearch-delete-knowledge.repository.ts
@@ -4,7 +4,8 @@ import { Elasticsearch } from "@/config/config";
 import { EmployeeRepository } from "@/domain/contracts/employee-repository.interface";
 import * as path from 'path';
 import * as fs from 'fs';
-
+import { promisify } from 'util';
+ 
 export class ElasticsearchDeleteKnowledgeRepository implements DeleteKnowledgeRepository {
   constructor (private readonly employeeRepository: EmployeeRepository) {}
     
@@ -13,31 +14,32 @@ export class ElasticsearchDeleteKnowledgeRepository implements DeleteKnowledgeRe
       hosts: [Elasticsearch.url]
     });
 
-    let knowledge = null
-    client.get({
+    const get = promisify(client.get).bind(client)
+
+    const response = await get({
       index: 'atomon',
       type: 'knowledge',
       id: knowledgeId
-    }, async (_error, response: any) => {
-      knowledge = response._source;
+    })
+    
+    const knowledge = response._source;
 
-      if (knowledge.attachments) {
-        for (const filename of knowledge.attachments) {
-          await this.employeeRepository.deleteAttachmentByFilename(knowledge.employeeId, filename)
-          try {
-            fs.unlinkSync(path.join(process.cwd(), 'tmp', filename))
-          } catch (error) {
-            console.error(`${filename} of employee ${knowledge.employeeId} is not found!`)
-          }
+    if (knowledge.attachments) {
+      for (const filename of knowledge.attachments) {
+        await this.employeeRepository.deleteAttachmentByFilename(knowledge.employeeId, filename)
+        try {
+          fs.unlinkSync(path.join(process.cwd(), 'tmp', filename))
+        } catch (error) {
+          console.error(`${filename} of employee ${knowledge.employeeId} is not found!`)
         }
       }
-      
-      await client.delete({
-        index: "atomon",
-        type: "knowledge",
-        id: knowledgeId
-      });
-    })
+    }
+
+    await client.delete({
+      index: "atomon",
+      type: "knowledge",
+      id: knowledgeId
+    });
 
   }
 }
